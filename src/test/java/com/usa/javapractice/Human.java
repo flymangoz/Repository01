fcc0be1da7194c8c56f0c6081d7f77cd91dce03a
flymangoz <mralam2099@gmail.com>
2023-04-06 13:43:17 -0400

    QA Rakib Alam

---
+++ src/test/java/com/usa/javapractice/Human.java
@@ -0,0 +1,22 @@
+package com.usa.javapractice;
+
+
+
+public class Human {
+
+	static int Green = 5000; // static variable/global/ class
+	
+	String Yellow = "Y1250"; // instance variable
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int cash = 50; // local variables
+		int water = 1000;
+	int wetcash = cash + water;
+	
+		System.out.println(cash + water / cash);
+		
+	
+	} 
+}
---
+++ src/test/java/com/usa/javapractice/Variable_declare.java
@@ -0,0 +1,17 @@
+package com.usa.javapractice;
+
+public class Variable_declare {
+
+	// how to declare a variable?
+   String name = "Rakib"; //instance variable
+	
+   
+   // how many types of variable?
+ //3 types of variable
+//instance
+//global/class/static
+//local
+
+}
+
+
